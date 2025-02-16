6b4e98c99bda73d91d3d53c5d24fb42e22479811
ninetailsrabbit <danieldigitalspace@proton.me>
2025-02-16 11:56:42 +0000

    better readable code on match 3 preview and auto update when parameters change

--- addons/ninetailsrabbit.match3_board/src/tools/match3_preview.gd
+++ addons/ninetailsrabbit.match3_board/src/tools/match3_preview.gd
@@ -13,34 +13,40 @@ class_name Match3Preview extends Node2D
 				remove_preview_cells()
 			
 @export_category("Editor Debug 🪲")
+@export var preview_pieces: Array[Texture2D] = []:
+	set(value):
+			preview_pieces = value
+			draw_preview_cells()
+			
 @export var preview_cell_texture: Texture2D:
 	set(value):
 		if value != preview_cell_texture:
 			preview_cell_texture = value
-		
-			if preview_cell_texture:
-				draw_preview_cells()
-			else:
-				remove_preview_cells()
-				
-@export var texture_scale: float = 1.0:
+			draw_preview_cells()
+
+@export var piece_texture_scale: float = 0.85:
 	set(value):
-		if value != texture_scale:
-			texture_scale = value
+		if value != piece_texture_scale:
+			piece_texture_scale = value
+			draw_preview_cells()
 		
-			if texture_scale:
-				draw_preview_cells()
-			else:
-				remove_preview_cells()
+@export var cell_texture_scale: float = 1.0:
+	set(value):
+		if value != cell_texture_scale:
+			cell_texture_scale = value
+			draw_preview_cells()
+		
+@export var position_font_size: int = 32:
+	set(value):
+		if value != position_font_size:
+			position_font_size = value
+			draw_preview_cells()
+			
 @export var display_cell_position: bool = true:
 	set(value):
 		if value != display_cell_position:
 			display_cell_position = value
-		
-			if display_cell_position:
-				draw_preview_cells()
-			else:
-				remove_preview_cells()
+			draw_preview_cells()
 
 
 func _enter_tree() -> void:
@@ -52,7 +58,7 @@ func _enter_tree() -> void:
 
 func remove_preview_cells() -> Match3Preview:
 	for child in get_children():
-			child.free()
+		child.free()
 			
 	return self
 		
@@ -61,32 +67,70 @@ func draw_preview_cells() -> Match3Preview:
 	if Engine.is_editor_hint() and board:
 		remove_preview_cells()
 			
-		var cells = []
-		
-		if cells.is_empty():
-			for column in board.configuration.grid_width:
-				cells.append([])
+		for column in board.configuration.grid_width:
+			for row in board.configuration.grid_height:
+				var cell := draw_preview_cell(column, row)
+				add_child(cell)
+				Match3BoardPluginUtilities.set_owner_to_edited_scene_root(cell)
 				
-				for row in board.configuration.grid_height:
-					var cell_sprite: Sprite2D = Sprite2D.new()
-					var label: Label = Label.new()
+				if Match3BoardPluginUtilities.remove_falsy_values(preview_pieces).size() > 0:
+					var piece :=  draw_preview_piece(cell, column, row)
+					add_child(piece)
+					Match3BoardPluginUtilities.set_owner_to_edited_scene_root(piece)
 					
-					if preview_cell_texture:
-						cell_sprite.texture = preview_cell_texture
-						var texture_size = cell_sprite.texture.get_size()
-						cell_sprite.scale = Vector2(board.configuration.cell_size.x / texture_size.x, board.configuration.cell_size.y / texture_size.y) * texture_scale
-					
-					cell_sprite.position = Vector2(board.configuration.cell_size.x * column, board.configuration.cell_size.y * row)
-					
-					if display_cell_position:
-						label.text = "(%d,%d)" % [column, row]
-					
-					cell_sprite.add_child(label)
-					add_child(cell_sprite)
-					Match3BoardPluginUtilities.set_owner_to_edited_scene_root(cell_sprite)
+				if display_cell_position:
+					var label := draw_label_cell_position(cell, column, row)
 					Match3BoardPluginUtilities.set_owner_to_edited_scene_root(label)
 					
 	return self
+	
+	
+func draw_preview_cell(column: int, row: int) -> Sprite2D:
+	if preview_cell_texture:
+		var cell_sprite: Sprite2D = Sprite2D.new()
+		
+		cell_sprite.name = "Cell(%d,%d)" % [column, row]
+		cell_sprite.texture = preview_cell_texture
+		var cell_texture_size = cell_sprite.texture.get_size()
+		cell_sprite.scale = Vector2(
+			board.configuration.cell_size.x / cell_texture_size.x, 
+			board.configuration.cell_size.y / cell_texture_size.y
+			) * cell_texture_scale
+	
+		cell_sprite.position = Vector2(board.configuration.cell_size.x * column, board.configuration.cell_size.y * row)
+		cell_sprite.z_index = 0
+		
+		return cell_sprite
+		
+	return null
+
+
+func draw_preview_piece(cell: Sprite2D, column: int, row: int) -> Sprite2D:
+	var piece_sprite: Sprite2D = Sprite2D.new()
+	piece_sprite.name = "Piece(%d,%d)" % [column, row]
+	
+	piece_sprite.texture = preview_pieces.pick_random()
+	var piece_texture_size = piece_sprite.texture.get_size()
+	piece_sprite.scale = Vector2(
+		board.configuration.cell_size.x / piece_texture_size.x, 
+		board.configuration.cell_size.y / piece_texture_size.y
+		) * piece_texture_scale
+	
+	piece_sprite.position = cell.position
+	piece_sprite.z_index = 1
+	
+	return piece_sprite
+	
+
+func draw_label_cell_position(cell: Sprite2D, column, row) -> Label:
+	var label: Label = Label.new()
+	label.z_index = 2
+	label.text = "(%d,%d)" % [column, row]
+	label.add_theme_font_size_override("font_size", position_font_size)
+	cell.add_child(label)
+	label.set_anchors_preset(Control.PRESET_CENTER)
+	
+	return label
 
 
 func _on_tool_button_pressed(text: String) -> void:
