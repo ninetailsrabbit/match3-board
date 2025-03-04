17aea0f668a6943abdbc5631e42a0db89e10b0e5
ninetailsrabbit <danieldigitalspace@proton.me>
2025-03-04 11:49:35 +0000

    renamed some parts and removed the updater

--- addons/ninetailsrabbit.match3_board/ninetailsrabbit.match3_board.gd
+++ addons/ninetailsrabbit.match3_board/ninetailsrabbit.match3_board.gd
@@ -1,19 +1,9 @@
 @tool
 extends EditorPlugin
 
-const UpdateNotifyToolScene = preload("updater/update_notify_tool.tscn")
-
-var update_notify_tool_instance: Node
 var inspector_plugin
 
-
 func _enter_tree() -> void:
-	MyPluginSettings.set_update_notification()
-	_setup_updater()
-	
-	if not DirAccess.dir_exists_absolute(MyPluginSettings.PluginTemporaryReleaseUpdateDirectoryPath):
-		DirAccess.make_dir_recursive_absolute(MyPluginSettings.PluginTemporaryReleaseUpdateDirectoryPath)
-	
 	inspector_plugin = preload("res://addons/ninetailsrabbit.match3_board/inspector/inspector_button_plugin.gd").new()
 	add_inspector_plugin(inspector_plugin)
 	
@@ -25,12 +15,6 @@ func _enter_tree() -> void:
 	
 
 func _exit_tree() -> void:
-	MyPluginSettings.remove_settings()
-	
-	if update_notify_tool_instance:
-		update_notify_tool_instance.free()
-		update_notify_tool_instance = null
-
 	remove_inspector_plugin(inspector_plugin)
 
 	remove_custom_type("Match3LineConnector")
@@ -38,10 +22,3 @@ func _exit_tree() -> void:
 	remove_custom_type("Match3Animator")
 	remove_custom_type("Match3BoardPreview")
 	remove_custom_type("Match3Board")
-	
-
-## Update tool referenced from https://github.com/MikeSchulze/gdUnit4/blob/master/addons/gdUnit4
-func _setup_updater() -> void:
-	if MyPluginSettings.is_update_notification_enabled():
-		update_notify_tool_instance = UpdateNotifyToolScene.instantiate()
-		Engine.get_main_loop().root.add_child.call_deferred(update_notify_tool_instance)
--- addons/ninetailsrabbit.match3_board/settings/plugin_settings.gd
+++ addons/ninetailsrabbit.match3_board/settings/plugin_settings.gd
@@ -1,5 +1,5 @@
 @tool
-class_name MyPluginSettings extends RefCounted
+class_name Match3BoardPluginSettings extends RefCounted
 
 const PluginPrefixName: String = "ninetailsrabbit.match3_board" ## The folder name
 const GitRepositoryName: String = "match3-board"
@@ -16,31 +16,11 @@ static var PluginTemporaryReleaseFilePath = "%s/%s.zip" % [PluginTemporaryDirect
 static var PluginDebugDirectoryPath = "res://debug"
 
 #region Plugin Settings
-static var UpdateNotificationSetting: String = PluginSettingsBasePath + "/update_notification_enabled"
+## PluginSettingsBasePath + "/update_notification_enabled"
 #endregion
 
 ## Enable to test the updater without need to have a latest release version to trigger it
 static var DebugMode: bool = false
-
-static func set_update_notification(enable: bool = ProjectSettings.get_setting(UpdateNotificationSetting, true)) -> void:
-	ProjectSettings.set_setting(UpdateNotificationSetting, enable)
-	ProjectSettings.add_property_info({
-		"name": UpdateNotificationSetting,
-		"type": typeof(enable),
-	 	"value": enable,
-		"hint": PROPERTY_HINT_TYPE_STRING,
-		"hint_string": "Turn notifications on or off to receive alerts when new versions of the plugin are released"
-	})
-	ProjectSettings.save()
-
-
-static func is_update_notification_enabled() -> bool:
-	return ProjectSettings.get_setting(UpdateNotificationSetting, true)
-
-
-static func remove_settings() -> void:
-	remove_setting(UpdateNotificationSetting)
-
 
 static func remove_setting(name: String) -> void:
 	if ProjectSettings.has_setting(name):
